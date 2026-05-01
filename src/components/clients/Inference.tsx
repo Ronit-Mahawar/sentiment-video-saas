e4a29421d8692ab7eac7fd6f7a2c415b7a5f57c7
Ronit-Mahawar <144982746+Ronit-Mahawar@users.noreply.github.com>
2026-05-01 12:42:39 +0530

    added overall analysis frontend

--- src/components/clients/Inference.tsx
+++ src/components/clients/Inference.tsx
@@ -1,19 +1,192 @@
-"use client"
+"use client";
 
-import UploadVideo from "./UploadVideo"
+import { useState, useEffect } from "react";
+import UploadVideo from "./UploadVideo";
+import { negative } from "zod/v4";
 
-interface InferenceProps{
-  quota:{
-    secretKey:string,
-  }
+const EMOTION_EMOJI: Record<string, string> = {
+  anger: "😠",
+  disjust: "🤢",
+  fear: "😨",
+  joy: "😄" ,
+  neutral: "😐",
+  sadness: "😢",
+  surprise: "😲",
+}
+const SENTIMENT_EMOJI: Record<string, string> = {
+  negative: "👎",
+  neutral: "😐",
+  positive: "👍",
 }
 
-export function Inference({quota} :InferenceProps){
-  return  <div className="flex h-fit w-full flex-col gap-3 md:w-1/2">
-    <h2 className="text-lg font-medium text-slate-800">Inference</h2>
-    <UploadVideo apiKey={quota.secretKey}/>
-   
 
+interface InferenceProps {
+  quota: {
+    secretKey: string;
+  };
+}
+
+export type Ananlysis = {
+  utternces: Array<{
+    start_time: number;
+    end_time: number;
+    text: string;
+    emotions: Array<{lable: string; confidence: number}>;
+    sentiments : Array<{lable: string; confidence: number}>;
+  }>;
+}
+
+export function Inference({ quota }: InferenceProps) {
+  const [analysis, setAnalysis] = useState<Ananlysis | null>(
+    {
+      utternces: [
+        {
+          start_time: 0,
+          end_time: 5,
+          text: "Hello everyone, welcome to today's presentation.",
+          emotions: [
+            { lable: "joy", confidence: 0.72 },
+            { lable: "neutral", confidence: 0.25 },
+          ],
+          sentiments: [
+            { lable: "positive", confidence: 0.68 },
+            { lable: "neutral", confidence: 0.20 },
+          ],
+        },
+        {
+          start_time: 5,
+          end_time: 10,
+          text: "I'm really excited to share this new product with you all.",
+          emotions: [
+            { lable: "joy", confidence: 0.85 },
+            { lable: "surprise", confidence: 0.12 },
+          ],
+          sentiments: [
+            { lable: "positive", confidence: 0.91 },
+          ],
+        },
+        {
+          start_time: 10,
+          end_time: 15,
+          text: "However, we did encounter some challenges during development.",
+          emotions: [
+            { lable: "fear", confidence: 0.34 },
+            { lable: "sadness", confidence: 0.28 },
+            { lable: "neutral", confidence: 0.35 },
+          ],
+          sentiments: [
+            { lable: "negative", confidence: 0.45 },
+            { lable: "neutral", confidence: 0.40 },
+          ],
+        },
+        {
+          start_time: 15,
+          end_time: 20,
+          text: "The team worked really hard to overcome these obstacles.",
+          emotions: [
+            { lable: "joy", confidence: 0.55 },
+            { lable: "neutral", confidence: 0.30 },
+          ],
+          sentiments: [
+            { lable: "positive", confidence: 0.62 },
+            { lable: "neutral", confidence: 0.25 },
+          ],
+        },
+        {
+          start_time: 20,
+          end_time: 25,
+          text: "Thank you for your attention.",
+          emotions: [
+            { lable: "neutral", confidence: 0.65 },
+            { lable: "joy", confidence: 0.30 },
+          ],
+          sentiments: [
+            { lable: "positive", confidence: 0.58 },
+            { lable: "neutral", confidence: 0.35 },
+          ],
+        },
+      ],
+    }
+  );
+
+
+
+  const getAverageScore = () => {
+    if(!analysis?.utternces.length) return null;
+
+    // Agregate all score 
+     const emotionScores: Record<string, number[]> = {};
+     const sentimentScores: Record<string, number[]> = {};
+
+      analysis.utternces.forEach((utterene)=>{
+        utterene.emotions.forEach((emotion)=>{    
+          if(!emotionScores[emotion.lable]) emotionScores[emotion.lable] = [];
+          emotionScores[emotion.lable]!.push(emotion.confidence);
+
+        })
+        utterene.sentiments.forEach((sentiment)=>{    
+          if(!sentimentScores[sentiment.lable]) sentimentScores[sentiment.lable] = [];
+          sentimentScores[sentiment.lable]!.push(sentiment.confidence);
+
+        })
+      })
+
+      //Calculate average
+      const avgEmotions = Object.entries(emotionScores).map(
+        ([lable, scores])=>({
+          lable,
+          confidence: scores.reduce((a,b)=>a+b,0)/scores.length,
+        }),
+      ); 
+      const avgSentiments = Object.entries(sentimentScores).map(
+        ([lable, scores])=>({
+          lable,
+          confidence: scores.reduce((a,b)=>a+b,0)/scores.length,
+        }),
+      ); 
+      // sort by confidence ,get top score
+      const topEmotion = avgEmotions.sort((a,b)=>b.confidence - a.confidence)[0];
+      const topSentiment = avgSentiments.sort((a,b)=>b.confidence - a.confidence)[0];
+
+      return {
+        topEmotion: topEmotion,
+        topSentiment: topSentiment,
+      };
+       
+
+  };
+   
+  const averages = getAverageScore();
+  return (
+    <div className="flex h-fit w-full flex-col gap-3 md:w-1/2">
+      <h2 className="text-lg font-medium text-slate-800">Inference</h2>
+      <UploadVideo onAnalysis={setAnalysis} apiKey={quota.secretKey} />
+      <h2 className="mt-2 text-sm text-slate-800">Overall Analysis</h2>
+      {true ? (
+        <div className="flex h-fit w-full flex-wrap items-center justify-center gap-4 rounded-xl border border-gray-200 p-4 sm:gap-8 sm:px-6">
+          <div className="flex flex-col items-center">
+            <span className="text-sm">Primary emotions</span>
+            <span className="text-[40px]">{EMOTION_EMOJI[averages?.topEmotion?.lable!]}</span>
+            <span className="text-sm text-gray-500">{averages?.topEmotion?.confidence.toFixed(3)}({(averages?.topEmotion?.confidence! * 100).toFixed(2)}%)</span>
+
+
+          </div>
+          <div className="flex flex-col items-center">
+            <span className="text-sm">Primary sentiments</span>
+            <span className="text-[40px]">{SENTIMENT_EMOJI[averages?.topSentiment?.lable!]}</span>
+            <span className="text-sm text-gray-500">{averages?.topSentiment?.confidence}({(averages?.topSentiment?.confidence! * 100).toFixed(2)}%)</span>
+
+
+          </div>
+        </div>
+      ) :(
+        <div className=" flex h-32 w-full items-center justify-center rounded-xl border border-dashed">
+          <span className="text-sm text-gray-400">Upload a video to see overall analysis</span>
+        </div>
+      )}
+      
+      
 
     </div>
+  );
 }
--- src/components/clients/UploadVideo.tsx
+++ src/components/clients/UploadVideo.tsx
@@ -1,38 +1,120 @@
-"use-client"
+"use-client";
 
-import { useState } from "react"
+import { useState } from "react";
 import { DiVim } from "react-icons/di";
 import { FiUpload } from "react-icons/fi";
+import { set } from "zod/v4";
+import type { Ananlysis } from "./Inference";
 
-interface UploadVideoProps{
-  apiKey:string,
+interface UploadVideoProps {
+  apiKey: string;
+  onAnalysis: (analysis: Ananlysis) => void;
 }
 
-function UploadVideo({apiKey}:UploadVideoProps){
-  const [state,setState]= useState<"idle"| "uploading" | "analysing">("idle")
+function UploadVideo({ apiKey, onAnalysis }: UploadVideoProps) {
+  const [status, setStatus] = useState<"idle" | "uploading" | "analysing">(
+    "idle",
+  );
+ 
+  const [error, setError] = useState<string | null>(null);
 
-  const [error,setError] = useState<string | null>("hello")
+  const handleUpload = async (file: File) => {
+    try {
+      setStatus("uploading");
+      setError(null);
+      const fileType = `.${file.name.split(".").pop()}`;
 
-  const handleUpload=async (file:File) => {};
+      // 1. Get upload URL
+      const res = await fetch("/api/upload-url", {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer " + apiKey,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ fileType: fileType }),
+      });
 
-  return <div className="flex w-full flex-col gap-2 ">
-    <div className="flex cursor-pointer flex-col items-center justify-center  gap-2 rounded-xl border border-dashed border-gray-300 p-10 ">
-       <input type="file" accept="video/mov,video/mp4,video/avi" className="hidden" id="video-upload"  onChange={(e) => {
-        const file = e.target.files?.[0];
-        if(file){
-          handleUpload(file)
-        }
-       }} />
-      <label htmlFor="video-upload" className="flex cursor-pointer h-20 w-full flex-col items-center ">
-        <FiUpload className="h-8 w-8 text-gray-500" />
-        <h3 className=" text-md w-30 mt-2 text-slate-800">{status=== "uploading" ? "Uploading..." : status=== "analysing" ? "Analysing..." : "Upload a Video"}</h3>
-        <p className="text-xs text-center text-gray-500">Get started with sentiment direction by uploading a video</p>
-      </label>
-      
+      if (!res.ok) {
+        const error = await res.json();
+        throw new Error(error?.error || "Failed to get upload URL");
+      }
 
+      const { url, fileId, key } = await res.json();
+
+      // 2. Upload file to S3
+      const uploadRes = await fetch(url, {
+        method: "PUT",
+        headers: { "Content-Type": file.type },
+        body: file.type,
+      });
+
+      if (!uploadRes.ok) {
+        throw new Error("Failed to upload file");
+      }
+
+      setStatus("analysing");
+
+      // 3. Analyze video
+      const analysisRes = await fetch("/api/sentiment-inference", {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer " + apiKey,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ key }),
+      });
+      if (!analysisRes.ok) {
+        const error = await analysisRes.json();
+        throw new Error(error?.error || "Failed to analyze video");
+      }
+
+      const analysis = await analysisRes.json();
+      console.log("Analysis result:", analysis);
+      onAnalysis(analysis);
+
+      setStatus("idle");
+    } catch (error) {
+      setError(
+        error instanceof Error ? error.message : "An unknown error occurred",
+      );
+    }
+  };
+
+  return (
+    <div className="flex w-full flex-col gap-2">
+      <div className="flex cursor-pointer flex-col items-center justify-center gap-2 rounded-xl border border-dashed border-gray-300 p-10">
+        <input
+          type="file"
+          accept="video/mov,video/mp4,video/avi"
+          className="hidden"
+          id="video-upload"
+          onChange={(e) => {
+            const file = e.target.files?.[0];
+            if (file) {
+              handleUpload(file);
+            }
+          }}
+        />
+        <label
+          htmlFor="video-upload"
+          className="flex h-20 w-full cursor-pointer flex-col items-center"
+        >
+          <FiUpload className="h-8 w-8 text-gray-500" />
+          <h3 className="text-md mt-2 w-30 text-slate-800">
+            {status === "uploading"
+              ? "Uploading..."
+              : status === "analysing"
+                ? "Analysing..."
+                : "Upload a Video"}
+          </h3>
+          <p className="text-center text-xs text-gray-500">
+            Get started with sentiment direction by uploading a video
+          </p>
+        </label>
+      </div>
+      {error && <div className="text-xs text-red-500">{error}</div>}
     </div>
-    {error && <div className="text-xs text-red-500">{error}</div>}
-    </div>
+  );
 }
 
 export default UploadVideo;
